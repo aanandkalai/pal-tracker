e97e5ea21aa2f9431f662448747ce2dd2b45216b
Anand <aanandkalai@gmail.com>
2019-05-08 13:13:38 -0600

    Database update

--- databases/tracker/create_databases.sql
+++ databases/tracker/create_databases.sql
@@ -1,10 +1,13 @@
 DROP DATABASE IF EXISTS tracker_dev;
 DROP DATABASE IF EXISTS tracker_test;
+DROP DATABASE IF EXISTS tracker_test1;
 
 CREATE DATABASE tracker_dev;
 CREATE DATABASE tracker_test;
+CREATE DATABASE tracker_test1;
 
 CREATE USER IF NOT EXISTS 'tracker'@'localhost'
   IDENTIFIED BY 'ford123';
 GRANT ALL PRIVILEGES ON tracker_dev.* TO 'tracker' @'localhost';
 GRANT ALL PRIVILEGES ON tracker_test.* TO 'tracker' @'localhost';
+GRANT ALL PRIVILEGES ON tracker_test1.* TO 'tracker' @'localhost';
